c15f35fffb9870878c6f87e9c1771e506b9a6438
goat5ucker <089pain@ro.ru>
2025-06-04 18:38:11 +0700

    setprecision

--- source/stage/write.hpp
+++ source/stage/write.hpp
@@ -11,12 +11,14 @@ void run(std::string filename, std::vector<gvec_2f> in_dots, std::vector<gvec_u2
 
     std::ofstream file_out(filepath);
     file_out << "Constellation " << filename << "(\n";
-    file_out << "{ // coords \n" << "    { " << in_dots[0].x << ", " << in_dots[0].y << ", 0. }";
+    file_out << "{ // coords \n" << "    { " << std::fixed << std::setprecision(5)
+                                << in_dots[0].x << ", " << in_dots[0].y << ", 0. }";
     for(int i = 1; i < in_dots.size(); i++)
     {
         file_out << ",\n    { " << in_dots[i].x << ", " << in_dots[i].y << ", 0. }";
     }
-    file_out << "\n},\n{ // indexes \n" << "    { " << in_indxs[0].a << ", " << in_indxs[0].b << " }";
+    file_out << "\n},\n{ // indexes \n" << "    { " << std::fixed << std::setprecision(5)
+                                 << in_indxs[0].a << ", " << in_indxs[0].b << " }";
     for(int i = 1; i < in_indxs.size(); i++)
     {
         file_out << ",\n    { " << in_indxs[i].a << ", " << in_indxs[i].b << " }";
